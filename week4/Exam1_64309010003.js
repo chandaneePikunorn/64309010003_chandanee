2af44d86b10bfdcdea392a5f640ff1f0df82f5a2
chandaneePikunorn <116250515+chandaneePikunorn@users.noreply.github.com>
2022-11-10 14:06:34 +0700

    Update Exam1_64309010003.js

--- week4/Exam1_64309010003.js
+++ week4/Exam1_64309010003.js
@@ -1,5 +1,5 @@
 const kelvin = 293;
 const celsius = kelvin - 273;
 let farenheit = celsius * (9 / 5) + 32;
-farenheit=Math.floor(farenheit);
+farenheit = Math.floor(farenheit);
 console.log(`The temperature is ${farenheit} degrees Fahrenheit.`);
