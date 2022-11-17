882568f20396551b304850cc9a41b36777ab2b06
chandaneePikunorn <116250515+chandaneePikunorn@users.noreply.github.com>
2022-11-17 10:06:06 +0700

    Create TesUS.js

---
+++ week4/TesUS.js
@@ -0,0 +1,9 @@
+console.log(5);
+var myName ='chandanee';
+let  lastName ='sun';
+const year =2022;
+//year =2023;
+let grade =3.56
+grade=Math.floor();
+console.log(grade);
+console.log(`My name is ${myName}`)
